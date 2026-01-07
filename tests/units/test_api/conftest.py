55546c388294e0b3be9c7362e6b196497e22dda3
Ilja Orlovs <VRGhost@users.noreply.github.com>
2026-01-07 11:54:42 +0000

    conftest

---
+++ tests/units/test_api/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+from financial_services_register_api.api import (
+    FinancialServicesRegisterApiSession,
+    FinancialServicesRegisterApiClient,
+)
+
+@pytest.fixture
+async def test_session(test_api_username, test_api_key):
+    session = FinancialServicesRegisterApiSession(test_api_username, test_api_key)
+    yield session
+    await session.aclose()
+
+
+@pytest.fixture
+async def test_client(test_api_username, test_api_key):
+    client = FinancialServicesRegisterApiClient(test_api_username, test_api_key)
+    yield client
+    await client.api_session.aclose()
