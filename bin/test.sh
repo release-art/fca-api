d1a6d2bbfb1a18ac0c42c8fbe8ac6ebe6f796b02
Ilja Orlovs <VRGhost@users.noreply.github.com>
2026-01-14 14:41:48 +0000

    env

--- bin/test.sh
+++ bin/test.sh
@@ -4,17 +4,18 @@ THIS_DIR=$(dirname "${BASH_SOURCE[0]}")
 PROJECT_ROOT=$(realpath "${THIS_DIR}/..")
 cd "${PROJECT_ROOT}"
 
-op run -- export
+export FCA_API_USERNAME="${FCA_API_USERNAME:-op://Employee/d5fon4y2ftqt6cb3bww4bizoza/username}"
+export FCA_API_KEY="${FCA_API_KEY:-op://Employee/d5fon4y2ftqt6cb3bww4bizoza/api key}"
 
-# exec pdm run pytest \
-#     --cache-clear \
-#     --capture=no \
-#     --code-highlight=yes \
-#     --color=yes \
-#     --cov=src \
-#     --cov-report=term-missing:skip-covered \
-#     -ra \
-#     -x \
-#     --tb=native \
-#     --verbosity=3 \
-#     "${@:-tests/}"
+exec op run --no-masking --  pdm run pytest \
+    --cache-clear \
+    --capture=no \
+    --code-highlight=yes \
+    --color=yes \
+    --cov=src \
+    --cov-report=term-missing:skip-covered \
+    -ra \
+    -x \
+    --tb=native \
+    --verbosity=3 \
+    "${@:-tests/}"
